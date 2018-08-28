7e9110ba8c82d51fe67f804b7edfb6eafa36f689
Heider1632 <36663423+Heider1632@users.noreply.github.com>
2018-08-28 11:25:36 -0500

    Add files via upload
    update app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+    <body className="body">
+    <main id="cd-main-content" className="main">
+      <section id="cd-intro" className="section-home">
+        <h1>Full Page Intro &amp; Navigation</h1>
+        <header className="cd-header">
+          <div id="cd-logo">
+            <a href="#0">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-logo_2.svg" alt="Logo" />
+            </a>
+          </div>
+          <a className="cd-menu-trigger" href="#main-nav">Menu<span></span></a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          <div className="cd-blurred-bg"></div>
+      </section>
+    </main>
+
+    <div className="cd-shadow-layer"></div>
+
+    <nav id="main-nav">
+      <ul>
+        <li><a href="#0"><span>Tour</span></a></li>
+        <li><a href="#0"><span>Pricing</span></a></li>
+        <li><a href="#0"><span>Labs</span></a></li>
+        <li><a href="#0"><span>About</span></a></li>
+        <li><a href="#0"><span>Contact us</span></a></li>
+      </ul>
+      <a href="#0" className="cd-close-menu">Close<span></span></a>
+    </nav>
+    </body>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import firebase from 'firebase';
+import './home-ani';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDpidiJZ_bbGfknCi0AYVmqVNXk5Vhh5Mw",
+    authDomain: "research-app-57a1a.firebaseapp.com",
+    databaseURL: "https://research-app-57a1a.firebaseio.com",
+    projectId: "research-app-57a1a",
+    storageBucket: "research-app-57a1a.appspot.com",
+    messagingSenderId: "1046793302242"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
